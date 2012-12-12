e680f450e3eb20ad34498400a56005fea91337d1
Christian Fredh <christian.fredh@gmail.com>
2012-12-12 16:20:39 +0100

    Added some stubs...

---
+++ ChessFramework/ChessFramework/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChessFramework
+{
+    public class Game
+    {
+        private Board _board = new Board();
+
+        public event EventHandler NewTurn;
+
+        public void Start()
+        {
+            NewTurn(this, EventArgs.Empty);
+        }
+
+        public void Resign()
+        {
+            
+        }
+
+        public void Move(Square from, Square to)
+        {
+            NewTurn(this, EventArgs.Empty);
+        }
+    }
+}
