34bb816e0b7cd8585336233392c4e9403d2d5b6f
Christian Fredh <christian.fredh@gmail.com>
2012-12-15 11:27:20 +0100

    Added SpecFlow.NUnit to Specs project and created first feature

---
+++ ChessFramework/ChessFramework.Specs/Features/GameStart.feature
@@ -0,0 +1,19 @@
+﻿Feature: Game Start
+	When the game start
+	I want the game set up correctly
+
+Scenario: Starting a game
+	Given a new game
+	When the game starts
+	Then the board should look like
+	|     |     |     |     |     |     |     |		|
+	| BRo | BKn | BBi | BQu | BKi | BBi | BKn | BRo |
+	| BPa | BPa | BPa | BPa | BPa | BPa | BPa | BPa |
+	|     |     |     |     |     |     |     |		|
+	|     |     |     |     |     |     |     |		|
+	|     |     |     |     |     |     |     |		|
+	|     |     |     |     |     |     |     |		|
+	| WPa | WPa | WPa | WPa | WPa | WPa | WPa | WPa |
+	| WRo | WKn | WBi | WQu | WKi | WBi | WKn | WRo |
+	And it should be White's turn
+	
---
+++ ChessFramework/ChessFramework.Specs/StepDefinitions/GameSteps.cs
@@ -0,0 +1,33 @@
+﻿using TechTalk.SpecFlow;
+
+namespace ChessFramework.Specs.StepDefinitions
+{
+    [Binding]
+    public class GameSteps
+    {
+        [Given(@"a new game")]
+        public void GivenANewGame()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the game starts")]
+        public void WhenTheGameStarts()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the board should look like")]
+        public void ThenTheBoardShouldLookLike(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"it should be White's turn")]
+        public void ThenItShouldBeWhiteSTurn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
