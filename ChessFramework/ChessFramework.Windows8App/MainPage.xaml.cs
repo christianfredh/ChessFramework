659c057e67c9532d08a2f07d8184f3c4b2052df6
Christian Fredh <christian.fredh@gmail.com>
2012-12-18 11:18:12 +0100

    Added UI for board and pieces

--- ChessFramework/ChessFramework.Windows8App/MainPage.xaml.cs
+++ ChessFramework/ChessFramework.Windows8App/MainPage.xaml.cs
@@ -81,6 +81,60 @@ namespace ChessFramework.Windows8App
             _squares.Add("f7", F7);
             _squares.Add("g7", G7);
             _squares.Add("h7", H7);
+
+            _squares.Add("a6", A6);
+            _squares.Add("b6", B6);
+            _squares.Add("c6", C6);
+            _squares.Add("d6", D6);
+            _squares.Add("e6", E6);
+            _squares.Add("f6", F6);
+            _squares.Add("g6", G6);
+            _squares.Add("h6", H6);
+
+            _squares.Add("a5", A5);
+            _squares.Add("b5", B5);
+            _squares.Add("c5", C5);
+            _squares.Add("d5", D5);
+            _squares.Add("e5", E5);
+            _squares.Add("f5", F5);
+            _squares.Add("g5", G5);
+            _squares.Add("h5", H5);
+
+            _squares.Add("a4", A4);
+            _squares.Add("b4", B4);
+            _squares.Add("c4", C4);
+            _squares.Add("d4", D4);
+            _squares.Add("e4", E4);
+            _squares.Add("f4", F4);
+            _squares.Add("g4", G4);
+            _squares.Add("h4", H4);
+
+            _squares.Add("a3", A3);
+            _squares.Add("b3", B3);
+            _squares.Add("c3", C3);
+            _squares.Add("d3", D3);
+            _squares.Add("e3", E3);
+            _squares.Add("f3", F3);
+            _squares.Add("g3", G3);
+            _squares.Add("h3", H3);
+
+            _squares.Add("a2", A2);
+            _squares.Add("b2", B2);
+            _squares.Add("c2", C2);
+            _squares.Add("d2", D2);
+            _squares.Add("e2", E2);
+            _squares.Add("f2", F2);
+            _squares.Add("g2", G2);
+            _squares.Add("h2", H2);
+
+            _squares.Add("a1", A1);
+            _squares.Add("b1", B1);
+            _squares.Add("c1", C1);
+            _squares.Add("d1", D1);
+            _squares.Add("e1", E1);
+            _squares.Add("f1", F1);
+            _squares.Add("g1", G1);
+            _squares.Add("h1", H1);
         }
 
         private void RenderBoard(Board board)
