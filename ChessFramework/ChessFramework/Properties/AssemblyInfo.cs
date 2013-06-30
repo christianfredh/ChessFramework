f2f4908353d7e29bab07c6959baf6366e554666e
Christian Fredh <christian.fredh@gmail.com>
2013-06-30 15:49:14 +0200

    Pre release versioning

--- ChessFramework/ChessFramework/Properties/AssemblyInfo.cs
+++ ChessFramework/ChessFramework/Properties/AssemblyInfo.cs
@@ -22,5 +22,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyInformationalVersion("1.0-alpha")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
