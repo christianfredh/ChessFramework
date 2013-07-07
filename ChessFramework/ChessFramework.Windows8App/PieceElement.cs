c20af0d5414f34a22b94b7f141873d6b61cead2c
Christian Fredh <christian.fredh@gmail.com>
2013-07-07 18:43:51 +0200

    Added some info to PieceElement

--- ChessFramework/ChessFramework.Windows8App/PieceElement.cs
+++ ChessFramework/ChessFramework.Windows8App/PieceElement.cs
@@ -6,10 +6,21 @@ namespace ChessFramework.Windows8App
 {
     public class PieceElement : Grid
     {
+        public Type PieceType { get; set; }
+        public Army Color { get; set; }
+
         public PieceElement(Piece piece)
         {
-            var uri = new Uri("ms-appx:/Assets/" + piece.Color + piece.GetType().Name + ".png", UriKind.Absolute);
-            Children.Add(new Image { Source = new BitmapImage(uri) });
+            PieceType = piece.GetType();
+            Color = piece.Color;
+
+            AddPieceImage();
+        }
+
+        private void AddPieceImage()
+        {
+            var uri = new Uri("ms-appx:/Assets/" + Color + PieceType.Name + ".png", UriKind.Absolute);
+            Children.Add(new Image {Source = new BitmapImage(uri)});
         }
     }
 }
