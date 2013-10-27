18874d3a503ee7c82858eb664685e3034d6ae78f
Christian Fredh <christian.fredh@gmail.com>
2013-10-27 16:35:11 +0100

    Increased version to alpha 2

--- ChessFramework/ChessFramework/Properties/AssemblyInfo.cs
+++ ChessFramework/ChessFramework/Properties/AssemblyInfo.cs
@@ -23,5 +23,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyInformationalVersion("1.0-alpha")]
+[assembly: AssemblyInformationalVersion("1.0-alpha2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
