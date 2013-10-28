50bbfde91b35a526dd522ed52a5891b672fd6438
Christian Fredh <christian.fredh@gmail.com>
2013-10-28 20:12:54 +0100

    Increased version number

--- ChessFramework/ChessFramework/Properties/AssemblyInfo.cs
+++ ChessFramework/ChessFramework/Properties/AssemblyInfo.cs
@@ -23,5 +23,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha5")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha6")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
